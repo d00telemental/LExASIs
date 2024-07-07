2f4e431a618f42b57f5acfef523902268b2d3544
d00telemental <4rterius@gmail.com>
2024-07-07 13:11:55 +0300

    implement manifest loading

--- LE3TextureMatch/Manifest.cpp
+++ LE3TextureMatch/Manifest.cpp
@@ -1,1 +1,234 @@
 #include "LE3TextureMatch/Manifest.hpp"
+
+namespace LExTextureMatch
+{
+
+    // ! Utilities.
+    // ========================================
+
+    template<std::size_t Length>
+    char const* FindChar(const char(&Haystack)[Length], char const Needle)
+    {
+        for (std::size_t i = 0; i < Length; ++i)
+        {
+            if (Haystack[i] == Needle)
+                return &(Haystack[i]);
+        }
+        return nullptr;
+    }
+
+
+    // ! Plain-old-data struct implementations.
+    // ========================================
+
+    bool CMipEntry::IsEmpty() const noexcept
+    {
+        return !IsExternal() && UnpackedSize == 0
+            && PackedSize == UINT32_MAX && PackedOffset == UINT32_MAX;
+    }
+
+    std::pair<std::uint16_t, std::uint16_t> CMipEntry::GetDimensions() const noexcept
+    {
+        return std::make_pair(Width, Height);
+    }
+
+    FString CTextureEntry::GetFullPath() const
+    {
+        LEASI_VERIFYA(FindChar(FullPath, '\0') != nullptr, "invalid full path", "");
+        return FString::Printf(L"%S", FullPath);
+    }
+
+    FString CTextureEntry::GetTfcName() const
+    {
+        LEASI_VERIFYA(FindChar(TfcName, '\0') != nullptr, "invalid tfc name", "");
+        return FString::Printf(L"%S", TfcName);
+    }
+
+    std::size_t CTextureEntry::GetMipCount() const
+    {
+        size_t i = 0;
+        for (; i < k_maxMipCount; ++i)
+        {
+            if (Mips[i].IsPlaceholder())
+                break;
+        }
+        return i;
+    }
+
+
+    // ! ManifestLoader implementation.
+    // ========================================
+
+    ManifestLoader::~ManifestLoader()
+    {
+        if (FileHandle != INVALID_HANDLE_VALUE || MappingHandle || View)
+        {
+            if (0 == UnmapViewOfFile(View))
+            {
+                auto const Error = FString::Printf(L"failed to unmap manifest view, error = %d", ::GetLastError());
+                OutputDebugStringW(*Error);
+                LEASI_WARN(L"{}", *Error);
+            }
+
+            if (0 == CloseHandle(MappingHandle))
+            {
+                auto const Error = FString::Printf(L"failed to close manifest mapping, error = %d", ::GetLastError());
+                OutputDebugStringW(*Error);
+                LEASI_WARN(L"{}", *Error);
+            }
+
+            if (0 == CloseHandle(FileHandle))
+            {
+                auto const Error = FString::Printf(L"failed to close manifest file, error = %d", ::GetLastError());
+                OutputDebugStringW(*Error);
+                LEASI_WARN(L"{}", *Error);
+            }
+
+            View = NULL;
+            MappingHandle = NULL;
+            FileHandle = INVALID_HANDLE_VALUE;
+        }
+    }
+
+    bool ManifestLoader::Load(std::wstring_view const InPath, FString& OutError)
+    {
+        LEASI_CHECKA(!InPath.empty(), "empty input path", "");
+
+        FileHandle = ::CreateFileW(InPath.data(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0u, NULL);
+        if (FileHandle == INVALID_HANDLE_VALUE)
+        {
+            OutError = FString::Printf(L"failed to open file, error = %d", ::GetLastError());
+            return false;
+        }
+
+        MappingHandle = ::CreateFileMappingW(FileHandle, NULL, PAGE_READONLY, 0u, 0u, NULL);
+        if (MappingHandle == NULL)
+        {
+            ::CloseHandle(std::exchange<HANDLE>(FileHandle, reinterpret_cast<HANDLE>(INVALID_HANDLE_VALUE)));
+            OutError = FString::Printf(L"failed to create file mapping, error = %d", ::GetLastError());
+            return false;
+        }
+
+        View = ::MapViewOfFile(MappingHandle, FILE_MAP_READ, 0u, 0u, 0u);
+        if (View == NULL)
+        {
+            ::CloseHandle(std::exchange(MappingHandle, reinterpret_cast<HANDLE>(NULL)));
+            ::CloseHandle(std::exchange(FileHandle, reinterpret_cast<HANDLE>(INVALID_HANDLE_VALUE)));
+            OutError = FString::Printf(L"failed to map view of file, error = %d", ::GetLastError());
+            return false;
+        }
+
+#define CLOSE_ERROR(...)                                                                        \
+    LEASI_WARN(__VA_ARGS__);                                                                    \
+    ::UnmapViewOfFile(std::exchange(View, reinterpret_cast<LPVOID>(NULL)));                     \
+    ::CloseHandle(std::exchange(MappingHandle, reinterpret_cast<HANDLE>(NULL)));                \
+    ::CloseHandle(std::exchange(FileHandle, reinterpret_cast<HANDLE>(INVALID_HANDLE_VALUE)));
+
+        LARGE_INTEGER OutFileSize{};
+        if (0 == GetFileSizeEx(FileHandle, &OutFileSize))
+        {
+            CLOSE_ERROR("failed to cache manifest file size");
+            return false;
+        }
+
+        CachedSize = OutFileSize.QuadPart;
+        if (CachedSize < sizeof(CManifestHeader))
+        {
+            CLOSE_ERROR("manifest file too small ({}) for header ({})", CachedSize, sizeof(CManifestHeader));
+            return false;
+        }
+
+        auto const Header = (CManifestHeader const*)View;
+        if (0 != std::memcmp(Header->Magic, CManifestHeader::k_checkMagic, sizeof(CManifestHeader::k_checkMagic))
+            || Header->Version > CManifestHeader::k_lastVersion)
+        {
+            CLOSE_ERROR("manifest file has invalid magic or version");
+            return false;
+        }
+
+        auto const EntryTableEnd = sizeof(CManifestHeader) + sizeof(CTextureEntry) * Header->TextureCount;
+        if (CachedSize < EntryTableEnd)
+        {
+            CLOSE_ERROR("entry table end ({}) out of manifest file bounds ({})", EntryTableEnd, CachedSize);
+            return false;
+        }
+
+        // TODO: This is an implicit requirement, we could try to force-align it.
+        LEASI_VERIFYW(EntryTableEnd % 4 == 0, L"entry table end not aligned", L"");
+
+        TextureMap.reserve(static_cast<std::size_t>(Header->TextureCount));
+        auto const* const TextureEntryStart = (CTextureEntry const*)((unsigned char const*)View + EntryTableEnd);
+
+        for (std::size_t i = 0; i < Header->TextureCount; ++i)
+        {
+            CTextureEntry const* const Entry = TextureEntryStart + i;
+            FString const EntryFullPath = Entry->GetFullPath();
+
+            LEASI_TRACE(L"adding manifest entry {} with {} mip(s) in texture file cache '{}'",
+                *EntryFullPath, Entry->GetMipCount(), *Entry->GetTfcName());
+
+            bool const bUniqueTexture = TextureMap.emplace(EntryFullPath, Entry).second;
+            if (!bUniqueTexture)
+            {
+                // This probably doesn't need to be a fatal error...
+                LEASI_WARN(L"manifest entry {} was not unique", *EntryFullPath);
+            }
+        }
+
+#undef CLOSE_ERROR
+
+        return true;
+    }
+
+    CTextureEntry const* ManifestLoader::FindEntry(FString const& FullPath) const
+    {
+        LEASI_CHECKA(FileHandle != INVALID_HANDLE_VALUE, "file not loaded", "");
+        LEASI_CHECKA(MappingHandle != NULL, "mapping not loaded", "");
+        LEASI_CHECKA(View != NULL, "view not loaded", "");
+
+        auto const Iter = TextureMap.find(FullPath);
+        if (Iter != TextureMap.end())
+            return Iter->second;
+
+        return nullptr;
+    }
+
+    ManifestLoader::ResolvedMip ManifestLoader::GetEntryMip(CTextureEntry const& InEntry, std::size_t const Index) const
+    {
+        LEASI_CHECKA(ValidateEntry(InEntry), "invalid entry provenance", "");
+        LEASI_VERIFYA(Index < InEntry.GetMipCount(), "mip index ({}) out of bounds ({})", Index, InEntry.GetMipCount());
+
+        ResolvedMip Resolved{};
+        Resolved.Entry = &InEntry.Mips[Index];
+
+        // This *should* not happen due to how GetMipCount() works...
+        LEASI_CHECKA(!Resolved.Entry->IsPlaceholder(), "can't retrieve placeholder mip record", "");
+
+        if (!Resolved.Entry->IsEmpty())
+        {
+            auto const [Offset, Count] = std::tie(Resolved.Entry->PackedOffset, Resolved.Entry->PackedSize);
+            Resolved.View = GetMappedView().subspan(static_cast<std::size_t>(Offset), static_cast<std::size_t>(Count));
+        }
+
+        return Resolved;
+    }
+
+    CManifestHeader const& ManifestLoader::GetMappedHeader() const
+    {
+        LEASI_CHECKA(FileHandle != INVALID_HANDLE_VALUE, "file not loaded", "");
+        LEASI_CHECKA(MappingHandle != NULL, "mapping not loaded", "");
+        LEASI_CHECKA(View != NULL, "view not loaded", "");
+
+        return *reinterpret_cast<CManifestHeader const*>(View);
+    }
+
+    std::span<unsigned char const> ManifestLoader::GetMappedView() const
+    {
+        LEASI_CHECKA(FileHandle != INVALID_HANDLE_VALUE, "file not loaded", "");
+        LEASI_CHECKA(MappingHandle != NULL, "mapping not loaded", "");
+        LEASI_CHECKA(View != NULL, "view not loaded", "");
+
+        auto const ViewPointer = reinterpret_cast<unsigned char const*>(View);
+        return std::span<unsigned char const>(ViewPointer, CachedSize);
+    }
+}
--- LE3TextureMatch/Manifest.hpp
+++ LE3TextureMatch/Manifest.hpp
@@ -1,77 +1,100 @@
 #pragma once
 
+#include <string_view>
+#include <unordered_map>
+#include <Windows.h>
 #include <LESDK/Headers.hpp>
 #include "LExCommon/Base.hpp"
 
 
+template<>
+struct std::hash<FString>
+{
+    std::size_t operator()(FString const& Key) const
+    {
+        // Reusing the TMap's hash implementation.
+        return static_cast<std::size_t>(GetTypeHash(Key));
+    }
+};
+
+
 namespace LExTextureMatch
 {
-	enum EPixelFormat : std::uint16_t
-	{
-		PF_Unknown						= 0,
-		PF_A32B32G32R32F                = 1,
-		PF_A8R8G8B8                     = 2,
-		PF_G8                           = 3,
-		PF_G16                          = 4,
-		PF_DXT1                         = 5,
-		PF_DXT3                         = 6,
-		PF_DXT5                         = 7,
-		PF_UYVY                         = 8,
-		PF_FloatRGB                     = 9,
-		PF_FloatRGBA                    = 10,
-		PF_DepthStencil                 = 11,
-		PF_ShadowDepth                  = 12,
-		PF_FilteredShadowDepth          = 13,
-		PF_R32F                         = 14,
-		PF_G16R16                       = 15,
-		PF_G16R16F                      = 16,
-		PF_G16R16F_FILTER               = 17,
-		PF_G32R32F                      = 18,
-		PF_A2B10G10R10                  = 19,
-		PF_A16B16G16R16_UNORM           = 20,
-		PF_D24                          = 21,
-		PF_R16F                         = 22,
-		PF_R16F_FILTER                  = 23,
-		PF_BC5                          = 24,
-		PF_V8U8                         = 25,
-		PF_A1                           = 26,
-		PF_NormalMap_LQ                 = 27,
-		PF_NormalMap_HQ                 = 28,
-		PF_A16B16G16R16_FLOAT           = 29,
-		PF_A16B16G16R16_SNORM           = 30,
-		PF_FloatR11G11B10               = 31,
-		PF_A4R4G4B4                     = 32,
-		PF_R5G6B5                       = 33,
-		PF_G8R8                         = 34,
-		PF_R8_UNORM                     = 35,
-		PF_R8_UINT                      = 36,
-		PF_R8_SINT                      = 37,
-		PF_R16_FLOAT                    = 38,
-		PF_R16_UNORM                    = 39,
-		PF_R16_UINT                     = 40,
-		PF_R16_SINT                     = 41,
-		PF_R8G8_UNORM                   = 42,
-		PF_R8G8_UINT                    = 43,
-		PF_R8G8_SINT                    = 44,
-		PF_R16G16_FLOAT                 = 45,
-		PF_R16G16_UNORM                 = 46,
-		PF_R16G16_UINT                  = 47,
-		PF_R16G16_SINT                  = 48,
-		PF_R32_FLOAT                    = 49,
-		PF_R32_UINT                     = 50,
-		PF_R32_SINT                     = 51,
-		PF_A8                           = 52,
-		PF_BC7                          = 53,
-		EPixelFormat_MAX                = 54
-	};
+    // ! Enumerations.
+    // ========================================
 
-    enum EMipFlags : std::uint16_t
+    enum EPixelFormat : std::uint32_t
     {
-        EMF_Placeholder					= 1 << 0,
-        EMF_External					= 1 << 1,
+        PF_Unknown                      = 0,
+        PF_A32B32G32R32F                = 1,
+        PF_A8R8G8B8                     = 2,
+        PF_G8                           = 3,
+        PF_G16                          = 4,
+        PF_DXT1                         = 5,
+        PF_DXT3                         = 6,
+        PF_DXT5                         = 7,
+        PF_UYVY                         = 8,
+        PF_FloatRGB                     = 9,
+        PF_FloatRGBA                    = 10,
+        PF_DepthStencil                 = 11,
+        PF_ShadowDepth                  = 12,
+        PF_FilteredShadowDepth          = 13,
+        PF_R32F                         = 14,
+        PF_G16R16                       = 15,
+        PF_G16R16F                      = 16,
+        PF_G16R16F_FILTER               = 17,
+        PF_G32R32F                      = 18,
+        PF_A2B10G10R10                  = 19,
+        PF_A16B16G16R16_UNORM           = 20,
+        PF_D24                          = 21,
+        PF_R16F                         = 22,
+        PF_R16F_FILTER                  = 23,
+        PF_BC5                          = 24,
+        PF_V8U8                         = 25,
+        PF_A1                           = 26,
+        PF_NormalMap_LQ                 = 27,
+        PF_NormalMap_HQ                 = 28,
+        PF_A16B16G16R16_FLOAT           = 29,
+        PF_A16B16G16R16_SNORM           = 30,
+        PF_FloatR11G11B10               = 31,
+        PF_A4R4G4B4                     = 32,
+        PF_R5G6B5                       = 33,
+        PF_G8R8                         = 34,
+        PF_R8_UNORM                     = 35,
+        PF_R8_UINT                      = 36,
+        PF_R8_SINT                      = 37,
+        PF_R16_FLOAT                    = 38,
+        PF_R16_UNORM                    = 39,
+        PF_R16_UINT                     = 40,
+        PF_R16_SINT                     = 41,
+        PF_R8G8_UNORM                   = 42,
+        PF_R8G8_UINT                    = 43,
+        PF_R8G8_SINT                    = 44,
+        PF_R16G16_FLOAT                 = 45,
+        PF_R16G16_UNORM                 = 46,
+        PF_R16G16_UINT                  = 47,
+        PF_R16G16_SINT                  = 48,
+        PF_R32_FLOAT                    = 49,
+        PF_R32_UINT                     = 50,
+        PF_R32_SINT                     = 51,
+        PF_A8                           = 52,
+        PF_BC7                          = 53,
+        EPixelFormat_MAX                = 54
     };
 
+    enum EMipFlags : std::uint32_t
+    {
+        EMF_Placeholder                 = 1 << 0,   // This mip and all records after it are absent (not "empty").
+        EMF_Original                    = 1 << 1,   // Mip should not be modified.
+        EMF_External                    = 1 << 2,   // Mip is located in a texture file cache.
+    };
+
+
+    // ! Plain-old-data structs.
+    // ========================================
+
 #pragma pack(push, 1)
+
     struct CManifestHeader
     {
         unsigned char   Magic[6];       // Magic bytes of 'LETEXM'.
@@ -88,24 +111,109 @@ namespace LExTextureMatch
 
     struct CMipEntry
     {
-        static constexpr size_t k_maxTfcLength = 24;
+        std::uint32_t   UnpackedSize;   // Number of bytes this mip occupies when fully uncompressed.
+        std::uint32_t   PackedSize;     // Number of bytes this mip currently occupies on the disk.
+        std::uint32_t   PackedOffset;   // Number of bytes from start of manifest to this mip contents.
+        std::uint16_t   Width;          // Size in horizontal dimension.
+        std::uint16_t   Height;         // Size in vertical dimension.
+        EMipFlags       Flags;          // Additional settings for this mip.
 
-        EMipFlags       Flags;
-        std::uint32_t   UnpackedSize;
-        std::uint32_t   PackedSize;
-        std::uint32_t   PackedOffset;
-        char            TfcName[k_maxTfcLength];
+        inline bool IsPlaceholder() const noexcept { return (Flags & EMF_Placeholder) != 0; }
+        inline bool IsOriginal() const noexcept { return (Flags & EMF_Original) != 0; }
+        inline bool IsExternal() const noexcept { return (Flags & EMF_External) != 0; }
+
+        /** Checks if this mip is "empty" (in LEX / MEM parlance). */
+        bool IsEmpty() const noexcept;
+        /** Retrieves mip dimensions as a tuple. */
+        std::pair<std::uint16_t, std::uint16_t> GetDimensions() const noexcept;
     };
 
     struct CTextureEntry
     {
-        static constexpr size_t k_maxIfpLength = 256;
-        static constexpr size_t k_maxMipCount = 16;
+        static constexpr std::size_t k_maxFullPathLength = 256;
+        static constexpr std::size_t k_maxTfcNameLength = 32;
+        static constexpr std::size_t k_maxMipCount = 16;
 
-        char            InstancedFullPath[k_maxIfpLength];  // Instanced full path of the Texture2D entry being replaced.
-        CMipEntry       Mips[k_maxMipCount];                // Mip records, their count and meta must match the original mips.
-		EPixelFormat	Format;								// Pixel format for all mips, must match LE definitions.
+        char            FullPath[k_maxFullPathLength];  // Full path of the Texture2D entry being matched (replaced).
+        char            TfcName[k_maxTfcNameLength];    // Name of the texture file cache used by external mips in this entry.
+        CMipEntry       Mips[k_maxMipCount];            // Mip records, their count and meta must match the original mips.
+        EPixelFormat    Format;                         // Pixel format for all mips, must match the LE definition.
+
+        /** Retrieves the matched Texture2D path as an Unreal string. */
+        FString GetFullPath() const;
+        /** Retrieves the texture file cache name as an Unreal string. */
+        FString GetTfcName() const;
+        /** Finds the actual number of mips. */
+        std::size_t GetMipCount() const;
     };
 
 #pragma pack(pop)
+
+
+    // ! Manifest types.
+    // ========================================
+
+    class ManifestLoader final : public NonCopyable
+    {
+        using TextureMap_t = std::unordered_map<FString, CTextureEntry const*>;
+
+        HANDLE          FileHandle{ INVALID_HANDLE_VALUE };
+        HANDLE          MappingHandle{ NULL };
+        LPVOID          View{ NULL };
+        SIZE_T          CachedSize{ 0 };
+        TextureMap_t    TextureMap{};
+
+    public:
+
+        ManifestLoader() = default;
+        ~ManifestLoader();
+
+        /**
+         * @brief       Attempts to initialize this manifest from a given file.
+         * @param[in]   InPath - path to the manifest file being loaded.
+         * @param[out]  OutError - receives error message if loading encounters an error.
+         * @return      Whether loading was successful.
+         */
+        bool Load(std::wstring_view InPath, FString& OutError);
+
+        struct ResolvedMip final
+        {
+            std::span<unsigned char const> View{};
+            CMipEntry const* Entry{};
+        };
+
+        /**
+         * @brief       Attempts to find a texture override entry by its @c FullPath .
+         * @param[in]   FullPath - full path of the looked-for texture override.
+         * @return      Pointer to the entry within the mapped memory view, or @c nullptr otherwise.
+         * @warning     This manifest must be loaded before calling this method.
+         */
+        CTextureEntry const* FindEntry(FString const& FullPath) const;
+
+        /**
+         * @brief       Retrieves mip level info and memory view.
+         * @param[in]   InEntry - texture override entry within the mapped memory view.
+         * @param[in]   Index - index of the mip level within the @c InEntry given.
+         * @return      Mip override entry, and the memory region it corresponds to if it's not empty.
+         * @warning     This manifest must be loaded before calling this method.
+         * @warning     The @c Index parameter must be within this entry's mip array bounds.
+         *              Call @ref CTextureEntry::GetMipCount() beforehand to verify.
+         */
+        ResolvedMip GetEntryMip(CTextureEntry const& InEntry, std::size_t Index) const;
+
+        /** Retrieves the manifest header from the start of mapped memory view. */
+        CManifestHeader const& GetMappedHeader() const;
+        /** Retrieves the mapped memory view. */
+        std::span<unsigned char const> GetMappedView() const;
+
+    private:
+
+        inline bool ValidateEntry(CTextureEntry const& Ref) const noexcept
+        {
+            auto const Pointer = reinterpret_cast<unsigned char const*>(&Ref);
+            auto const Start = reinterpret_cast<unsigned char const*>(View);
+            return Pointer >= Start && Pointer < Start + CachedSize;
+        }
+    };
+
 }
